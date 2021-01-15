594847337878745ba1275206cc6185dab51f7e19
RachelAbbie <rachelabbief@gmail.com>
2021-01-15 17:18:18 +0000

    Added icons in the header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
       <h1>Let's build the facebook clone!</h1>
+      <Header />
+      {/* App body*/}
+       {/* Sidebar */}
+       {/* Feed */}
+       {/* Widgets */}
+
     </div>
   );
 }
